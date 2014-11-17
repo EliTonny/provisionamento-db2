a3d8d5bb86249c4316c4ac4bf7f09e1e214e72ef
eli.t.desouza <eli.t.desouza@gmail.com>
2014-11-17 01:39:55 +0000

    Finalizadas as classes de DAO

--- bd2-provisionamento/src/DaoMongoDB/DaoMongoGrupoComunitario.java
+++ bd2-provisionamento/src/DaoMongoDB/DaoMongoGrupoComunitario.java
@@ -2,10 +2,18 @@ package DaoMongoDB;
 
 import MyExceptions.DaoException;
 import Sistema.Dao;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.bson.types.ObjectId;
+import provisionamento.model.Categoria;
 import provisionamento.model.GrupoComunitario;
+import provisionamento.model.Participante;
+import provisionamento.model.Usuario;
 
 public class DaoMongoGrupoComunitario implements Dao<GrupoComunitario> {
 
@@ -29,28 +37,70 @@ public class DaoMongoGrupoComunitario implements Dao<GrupoComunitario> {
     }
     
     @Override
-    public void grava(GrupoComunitario objeto) throws DaoException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void grava(GrupoComunitario grupoComunitario) throws DaoException {
+        BasicDBObject doc = new BasicDBObject()
+                .append("_id", grupoComunitario.getId())
+                .append("nmComprador", grupoComunitario.getComprador().id)
+                .append("dsGrupoComunitario", grupoComunitario.getDescricao())
+                .append("dsGrupoComunitarioUpper", grupoComunitario.getDescricao().trim().toUpperCase())
+                .append("categoria", grupoComunitario.getCategoria())
+                .append("criador",grupoComunitario.getCriador())
+                .append("dtCriacao", grupoComunitario.getDataCriacao())
+                .append("prazoValidade", grupoComunitario.getPrazoValidade())
+                .append("diasNotificacao", grupoComunitario.getQrdDiasNotificacao())
+                .append("quantidade", grupoComunitario.getQuantidade())
+                .append("vlrCompra", grupoComunitario.getValorCompra())
+                .append("participantes", grupoComunitario.getParticipantes())
+                .append("pago", grupoComunitario.isPago());
+        collection.insert(doc);
+        grupoComunitario.id = doc.getObjectId("_id");
     }
 
     @Override
-    public void deleta(GrupoComunitario objeto) throws DaoException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void deleta(GrupoComunitario grupoComunitario) throws DaoException {
+        BasicDBObject query = new BasicDBObject("_id", grupoComunitario.getId());
+        collection.remove(query);
     }
 
     @Override
     public GrupoComunitario busca(String nome) throws DaoException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        BasicDBObject query = new BasicDBObject("dsGrupoComunitarioUpper", nome.trim().toUpperCase());
+        return DBObjectToGrupoComunitario(collection.findOne(query));
     }
 
     @Override
     public GrupoComunitario busca(ObjectId id) throws DaoException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        BasicDBObject query = new BasicDBObject("_id", id);
+        return DBObjectToGrupoComunitario(collection.findOne(query));          
     }
 
     @Override
     public List<GrupoComunitario> busca() throws DaoException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<GrupoComunitario> grupos = new ArrayList();
+        try (DBCursor cursor = collection.find()) {
+            while (cursor.hasNext()) {
+                grupos.add(DBObjectToGrupoComunitario(cursor.next()));
+            }
+        }
+        return grupos;
+    }
+    
+    private GrupoComunitario DBObjectToGrupoComunitario(DBObject ob){
+        GrupoComunitario grupo = new GrupoComunitario();
+        grupo.setId((ObjectId)ob.get("_id"));
+        grupo.setComprador((Integer) ob.get("nmComprador"));
+        grupo.setDescricao((String) ob.get("dsGrupoComunitario"));
+        grupo.setCategoria((Categoria) ob.get("categoria"));
+        grupo.setCriador((Usuario) ob.get("criador"));
+        grupo.setDataCriacao((Date) ob.get("dtCriacao"));
+        grupo.setPrazoValidade((Date) ob.get("prazoValidade"));
+        grupo.setQrdDiasNotificacao((Integer) ob.get("diasNotificacao"));
+        grupo.setValorCompra((Double) ob.get("vlrCompra"));
+        grupo.setQuantidade((Integer) ob.get("quantidade"));
+        grupo.setParticipantes((ArrayList<Participante>) ob.get("participantes"));
+        grupo.setPago((Boolean) ob.get("pago"));
+        
+        return grupo;
     }
     
 }
--- bd2-provisionamento/src/DaoMongoDB/DaoMongoGrupoUnitario.java
+++ bd2-provisionamento/src/DaoMongoDB/DaoMongoGrupoUnitario.java
@@ -2,10 +2,17 @@ package DaoMongoDB;
 
 import MyExceptions.DaoException;
 import Sistema.Dao;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.bson.types.ObjectId;
+import provisionamento.model.Categoria;
 import provisionamento.model.GrupoUnitario;
+import provisionamento.model.Usuario;
 
 public class DaoMongoGrupoUnitario implements Dao<GrupoUnitario>  {
 
@@ -29,28 +36,65 @@ public class DaoMongoGrupoUnitario implements Dao<GrupoUnitario>  {
     }
     
     @Override
-    public void grava(GrupoUnitario objeto) throws DaoException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void grava(GrupoUnitario grupoUnitario) throws DaoException {
+        BasicDBObject doc = new BasicDBObject()
+                .append("_id", grupoUnitario.getId())
+                .append("dsGrupoUnitario", grupoUnitario.getDescricao())
+                .append("dsGrupoUnitarioUpper", grupoUnitario.getDescricao().trim().toUpperCase())
+                .append("categoria", grupoUnitario.getCategoria())
+                .append("criador",grupoUnitario.getCriador())
+                .append("dtCriacao", grupoUnitario.getDataCriacao())
+                .append("prazoValidade", grupoUnitario.getPrazoValidade())
+                .append("diasNotificacao", grupoUnitario.getQrdDiasNotificacao())
+                .append("quantidade", grupoUnitario.getQuantidade())
+                .append("vlrCompra", grupoUnitario.getValorCompra());
+        collection.insert(doc);
+        grupoUnitario.id = doc.getObjectId("_id");
     }
 
     @Override
-    public void deleta(GrupoUnitario objeto) throws DaoException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void deleta(GrupoUnitario grupoUnitario) throws DaoException {
+        BasicDBObject query = new BasicDBObject("_id", grupoUnitario.getId());
+        collection.remove(query);
     }
 
     @Override
     public GrupoUnitario busca(String nome) throws DaoException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        BasicDBObject query = new BasicDBObject("dsGrupoUnitarioUpper", nome.trim().toUpperCase());
+        return DBObjectToGrupoUnitario(collection.findOne(query));
     }
 
     @Override
     public GrupoUnitario busca(ObjectId id) throws DaoException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        BasicDBObject query = new BasicDBObject("_id", id);
+        return DBObjectToGrupoUnitario(collection.findOne(query));
     }
 
     @Override
     public List<GrupoUnitario> busca() throws DaoException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ArrayList<GrupoUnitario> grupos = new ArrayList();
+        try (DBCursor cursor = collection.find()) {
+            while (cursor.hasNext()) {
+                grupos.add(DBObjectToGrupoUnitario(cursor.next()));
+            }
+        }
+        return grupos;
+    }
+    
+    private GrupoUnitario DBObjectToGrupoUnitario(DBObject ob){
+        GrupoUnitario grupo = new GrupoUnitario();
+
+        grupo.setId((ObjectId)ob.get("_id"));
+        grupo.setDescricao((String) ob.get("dsGrupoComunitario"));
+        grupo.setCategoria((Categoria) ob.get("categoria"));
+        grupo.setCriador((Usuario) ob.get("criador"));
+        grupo.setDataCriacao((Date) ob.get("dtCriacao"));
+        grupo.setPrazoValidade((Date) ob.get("prazoValidade"));
+        grupo.setQrdDiasNotificacao((Integer) ob.get("diasNotificacao"));
+        grupo.setValorCompra((Double) ob.get("vlrCompra"));
+        grupo.setQuantidade((Integer) ob.get("quantidade"));
+        
+        return grupo;
     }
     
 }
